9a5e36fb6ea08a988d995265c043977437ec0959
AmirH031 <aamirshussain03@gmail.com>
2025-06-24 10:53:24 +0530

    Updates

---
+++ assets/createLucideIcon-BHDkDqnO.js
@@ -0,0 +1,27 @@
+import{j as e}from"./ui-vendor-C5dm8rnt.js";import{r}from"./react-vendor-BiErcIuD.js";import{c as a}from"./index-DPNeYU5-.js";const s=r.forwardRef(({className:r,...s},o)=>e.jsx("div",{ref:o,className:a("rounded-xl border bg-card text-card-foreground shadow",r),...s}));s.displayName="Card";const o=r.forwardRef(({className:r,...s},o)=>e.jsx("div",{ref:o,className:a("flex flex-col space-y-1.5 p-6",r),...s}));o.displayName="CardHeader";const t=r.forwardRef(({className:r,...s},o)=>e.jsx("div",{ref:o,className:a("font-semibold leading-none tracking-tight",r),...s}));t.displayName="CardTitle";r.forwardRef(({className:r,...s},o)=>e.jsx("div",{ref:o,className:a("text-sm text-muted-foreground",r),...s})).displayName="CardDescription";const d=r.forwardRef(({className:r,...s},o)=>e.jsx("div",{ref:o,className:a("p-6 pt-0",r),...s}));d.displayName="CardContent";r.forwardRef(({className:r,...s},o)=>e.jsx("div",{ref:o,className:a("flex items-center p-6 pt-0",r),...s})).displayName="CardFooter";
+/**
+ * @license lucide-react v0.474.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+const i=(...e)=>e.filter((e,r,a)=>Boolean(e)&&""!==e.trim()&&a.indexOf(e)===r).join(" ").trim();
+/**
+ * @license lucide-react v0.474.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+var l={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};
+/**
+ * @license lucide-react v0.474.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=r.forwardRef(({color:e="currentColor",size:a=24,strokeWidth:s=2,absoluteStrokeWidth:o,className:t="",children:d,iconNode:c,...n},m)=>r.createElement("svg",{ref:m,...l,width:a,height:a,stroke:e,strokeWidth:o?24*Number(s)/Number(a):s,className:i("lucide",t),...n},[...c.map(([e,a])=>r.createElement(e,a)),...Array.isArray(d)?d:[d]])),n=(e,a)=>{const s=r.forwardRef(({className:s,...o},t)=>{return r.createElement(c,{ref:t,iconNode:a,className:i(`lucide-${d=e,d.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase()}`,s),...o});var d});return s.displayName=`${e}`,s};
+/**
+ * @license lucide-react v0.474.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */export{s as C,d as a,o as b,n as c,t as d};
---
+++ assets/label-C3Jht1rg.js
@@ -0,0 +1,7 @@
+import{c as e}from"./createLucideIcon-BHDkDqnO.js";import{j as a,a as s}from"./ui-vendor-C5dm8rnt.js";import{r as o}from"./react-vendor-BiErcIuD.js";import{a as r}from"./utils-vendor-Bmne0XXo.js";import{c as t}from"./index-DPNeYU5-.js";
+/**
+ * @license lucide-react v0.474.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=e("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);var l=o.forwardRef((e,o)=>a.jsx(s.label,{...e,ref:o,onMouseDown:a=>{a.target.closest("button, input, select, textarea")||(e.onMouseDown?.(a),!a.defaultPrevented&&a.detail>1&&a.preventDefault())}}));l.displayName="Label";var n=l;const i=r("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),m=o.forwardRef(({className:e,...s},o)=>a.jsx(n,{ref:o,className:t(i(),e),...s}));m.displayName=n.displayName;export{d as C,m as L};
